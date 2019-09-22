ac170a1ea4cbd54ad4e20341d91f0bf101d95522
lgpinguim <lg_pinguim@hotmail.com>
2019-09-22 20:18:11 -0300

    A lot of things

---
+++ New Unity Project/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public string primeiroNivel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(primeiroNivel);
+    }
+
+    public void optionsMenu()
+    {
+
+    }
+
+    public void closeOptions()
+    {
+
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
