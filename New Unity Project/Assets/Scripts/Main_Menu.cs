3168f4b2dc8dea96daeb0af17d9578aa6a1b6cb9
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-09-30 20:59:11 -0300

    Soundtrack added

--- New Unity Project/Assets/Scripts/Main_Menu.cs
+++ New Unity Project/Assets/Scripts/Main_Menu.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Main_Menu : MonoBehaviour
 {
     public string primeiroNivel;
+    Image image;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = GetComponent<Image>();
+
+        Color c = image.color;
+        c.a = 0;
+        image.color = c;
+
     }
 
     // Update is called once per frame
