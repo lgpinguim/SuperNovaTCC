6f6033e3a61bc744ef3adeb94d597fbf54a68467
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-10-04 20:50:39 -0300

    COmiit particle explosion

--- New Unity Project/Assets/Scripts/CollisionHandler.cs
+++ New Unity Project/Assets/Scripts/CollisionHandler.cs
@@ -6,19 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 
-    
+
 {
-    [SerializeField]
-    float levelLoadDelay = 1f;
+    [Tooltip("InSeconds")]
+    [SerializeField] float levelLoadDelay = 1f;
+
+    [Tooltip("FX Prefab On Player")]
+    [SerializeField] GameObject deathFX;
 
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
+
     }
 
     private void StartDeathSequence()
     {
-        
+
         SendMessage("OnPlayerDeath");
     }
+
+    private void ReloadScene() // string reference
+    {
+        SceneManager.LoadScene(1);
+    }
+
 }
