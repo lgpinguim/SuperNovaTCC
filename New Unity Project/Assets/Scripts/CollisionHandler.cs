adeee91a57ca43a456cf6103b2d34784bbc08ed8
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-10-03 22:07:29 -0300

    Criaçao de scripts

---
+++ New Unity Project/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Player Trigger");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
