f99f1f138989b34cd8dfa0709168cbaa31f128ee
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-10-11 20:52:22 -0300

    Need to Fix The explosions

---
+++ New Unity Project/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject EnemyDeathFX;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
+        sphereCollider.radius = 1.5f;
+        sphereCollider.isTrigger = false;
+     
+      
+        
+    }
+
+    // Update is called once per frame
+
+
+    void OnParticleCollision(GameObject other)
+    {
+        Instantiate(EnemyDeathFX,transform.position,Quaternion.identity);
+       
+        Destroy(gameObject);
+    }
+}
