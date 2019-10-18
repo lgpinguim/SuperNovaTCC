25f6f69b7431c2729d290bfe16e97762177296ce
lgpinguim <37229605+lgpinguim@users.noreply.github.com>
2019-10-17 21:08:10 -0300

    Added score board

---
+++ New Unity Project/Assets/ScoreBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 5;
+
+    int score;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+
+    }
+
+    public void ScoreHit()
+    {
+        score = score + scorePerHit;
+
+
+    }
+}
